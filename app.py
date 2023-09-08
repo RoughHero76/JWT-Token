9152c5d95fccfee0d4a62c1074257164d8ac3998
Faizan Khan <m.faisalkhan9@gmail.com>
2023-09-08 12:44:32 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -1,7 +1,72 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, request, render_template, jsonify, send_from_directory
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from werkzeug.utils import secure_filename
+
 
 app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'your-secret-key'
+jwt = JWTManager(app)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["20 per minute"]
+)
+
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username == 'admin' and password == 'password':
+        access_token = create_access_token(identity=username)
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify(message='Authentication failed'), 401
+
+@app.route('/upload', methods=['POST'])
+@jwt_required()
+def upload_image():
+    if 'file' not in request.files:
+        return jsonify(message='No file part'), 400
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify(message='No selected file'), 400
+
+    if file:
+        # Securely save the uploaded file
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+        try:
+            file.save(file_path)
+            print(f"Saved file as {file_path}")  # Debugging: Check if file is saved correctly
+            return jsonify(message='Image uploaded successfully', image_name=filename)
+        except Exception as e:
+            print(f"Error saving file: {str(e)}")  # Debugging: Print any exceptions that occur
+            return jsonify(message='Error saving file'), 500
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/list_uploaded_images')
+@limiter.request_filter
+def list_uploaded_images():
+    uploaded_images = os.listdir(app.config['UPLOAD_FOLDER'])
+    return jsonify(uploaded_images=uploaded_images)
+
+if __name__ == '__main__':
+    app.run(debug=True)
